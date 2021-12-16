8cd5f526beff6c6ba4dd646848f1657bded288c2
Denis Fisenko <denis.fisenko@microsoft.com>
2021-12-16 12:25:43 -0600

    Initial Commit

---
+++ SampleFrameworkTwo/SampleFrameworkTwo.h
@@ -0,0 +1,18 @@
+//
+//  SampleFrameworkTwo.h
+//  SampleFrameworkTwo
+//
+//  Created by Denis Fisenko on 12/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleFrameworkTwo.
+FOUNDATION_EXPORT double SampleFrameworkTwoVersionNumber;
+
+//! Project version string for SampleFrameworkTwo.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkTwoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFrameworkTwo/PublicHeader.h>
+
+
